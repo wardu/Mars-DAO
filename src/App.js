b6d4b620575872b01431ec9abd857f4a173d70a0
Warren Dubery <warrendubery@protonmail.ch>
2022-11-08 18:54:47 +0000

    Add functionality to view more details on a single poll

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header/Header";
-import Polling from "./components/Polling/Polling";
-import Forum from "./components/Forum/Forum";
 import Login from "./components/Login/Login";
-import Footer from "./components/Footer/Footer";
+import Forum from "./components/Forum/Forum";
+import Polling from "./components/Polling/Polling";
+import PollDetails from "./components/PollDetails/PollDetails";
 import AddPoll from "./components/AddPoll/AddPoll";
+import Footer from "./components/Footer/Footer";
 
 // Styles
 import "./styles/partials/_resets.scss";
@@ -20,6 +21,7 @@ function App() {
         <Route path='/' element={<Login />} />
         <Route path='/forum' element={<Forum />} />
         <Route path='/polling' element={<Polling />} />
+        <Route path='/polling/:pollId' element={<PollDetails />} />
         <Route path='/polling/add' element={<AddPoll />} />
       </Routes>
       <Footer />
--- src/components/PollDetails/PollDetails.jsx
+++ src/components/PollDetails/PollDetails.jsx
@@ -1,62 +1,78 @@
-import axios from "axios";
-import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import axios from "axios";
+import "./PollDetails.scss";
+import { Link, useParams } from "react-router-dom";
 import clockIcon from "../../assets/icons/clock.svg";
 
-import "./Polling.scss";
-
-const Polling = () => {
+const PollDetails = () => {
   const tomorrow = new Date();
   tomorrow.setDate(tomorrow.getDate() + 1);
-
   const [pollEnd, setPollEnd] = useState(tomorrow.setHours(0, 0, 0, 0));
 
-  const [polls, setPolls] = useState([]);
-
-  // get pollEnd in format NOV 03 2022 16:00 UTC
-  const getPollEnd = () => {
-    const date = new Date(pollEnd).toLocaleString("default");
-    return `${date}    UTC`;
-  };
-
-  // function to get the difference between the current time and the poll end time
-  const getTimeDifference = () => {
-    const now = new Date().getTime();
-    const difference = pollEnd - now;
-    return difference;
-  };
-
-  // Gets the Polls data upon load
-  const getPolls = async () => {
-    const response = await axios.get("http://localhost:8080/polling");
-    setPolls(response.data);
-  };
-  useEffect(() => {
-    getPolls();
-  }, []);
+  const [poll, setPoll] = useState(null);
+  const { pollId } = useParams();
 
   const convertToHoursAndMinutes = (timestamp) => {
     const hours = Math.floor(timestamp / (1000 * 60 * 60));
     const minutes = Math.floor((timestamp % (1000 * 60 * 60)) / (1000 * 60));
     return `${hours}h ${minutes}m`;
   };
-
+  const getTimeDifference = () => {
+    const now = new Date().getTime();
+    const difference = pollEnd - now;
+    return difference;
+  };
+  const getPollEnd = () => {
+    const date = new Date(pollEnd).toLocaleString("default");
+    return `${date}    UTC`;
+  };
   // functionconvert unix timestamp to date
   const convertUnixToDate = (timestamp) => {
     const date = new Date(timestamp).toLocaleString("default");
     return date;
   };
 
-  const pollsList = polls.map((poll) => (
-    <article key={poll.id} className='poll'>
+  const commentSubmitHandler = async (e) => {
+    e.preventDefault();
+    console.log(e.comment);
+    const comment = e.target.value;
+    const response = await axios.post(
+      `http://localhost:8080/polling/${pollId}/comments`,
+      comment
+    );
+    setPoll(response.data);
+  };
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/polling/${pollId}`)
+      .then((res) => {
+        setPoll(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (!poll) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <article className='poll'>
+      <Link to='/polling' className='itemDetails__header--back'>
+        <p className='poll__back-button'> ← Back</p>
+      </Link>
       <div className='poll__time-remaining'>
         <img src={clockIcon} alt='Icon of a clock face' />
         <p>{convertToHoursAndMinutes(getTimeDifference())} REMAINING</p>
       </div>
+
       <div className='poll__details'>
         <p>POSTED {convertUnixToDate(parseInt(poll.pollDate))} - UTC</p>
-        <p>POLL ID 182</p>
+        <p>{poll.id}</p>
       </div>
+
       <h3 className='poll__title'>{poll.pollTitle}</h3>
       <p className='poll__body'>{poll.pollBody}</p>
       <section className='poll__lower'>
@@ -71,9 +87,6 @@ const Polling = () => {
             })}
           </div>
 
-          <Link to={`/polling/${poll.id}`}>
-            <div className='poll__view-details'>View Details</div>
-          </Link>
           <p className='vote-bar__title'>Results so far:</p>
           <div className='vote-bar'>
             <div
@@ -86,7 +99,6 @@ const Polling = () => {
                   "%",
               }}
             >
-              {/* style={{marginRight: spacing + 'em'}} */}
               {poll.voteFor}
             </div>
             <div
@@ -115,27 +127,25 @@ const Polling = () => {
             </div>
           </div>
         </div>
+        <form className='poll__comment-form'>
+          <label htmlFor='comment'>Comment on this proposal</label>
+          <textarea
+            className='poll__comment-form-input'
+            name='comment'
+            id='comment'
+            cols='30'
+            rows='10'
+          ></textarea>
+          <div
+            className='poll__comment-form-button'
+            onClick={commentSubmitHandler}
+          >
+            <p>Submit</p>
+          </div>
+        </form>
       </section>
     </article>
-  ));
-
-  return (
-    <section className='polls'>
-      <div className='poll__add-new'>
-        <Link to='/polling/add'>Add New Poll</Link>
-      </div>
-      <div className='polls__title'>
-        <h1>Active Polls</h1>
-      </div>
-      <div className='polls__subtitle'>
-        <h2>
-          {polls.length} POLLS ENDING {getPollEnd(pollEnd)}
-        </h2>
-      </div>
-
-      <div className='polls-container'>{pollsList}</div>
-    </section>
   );
 };
 
-export default Polling;
+export default PollDetails;
